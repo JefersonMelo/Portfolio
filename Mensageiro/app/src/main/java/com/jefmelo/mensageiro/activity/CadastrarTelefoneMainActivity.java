eb5f6081a715ca6e6da1f0a8ed9fd56c5dd09148
Jeferson Melo <jefsdemelo@gmail.com>
2021-06-18 13:36:05 -0300

    commit envio e recebimento de token ok. trabalhando na otimização da autenticação

--- Mensageiro/app/src/main/java/com/jefmelo/mensageiro/activity/CadastrarTelefoneMainActivity.java
+++ Mensageiro/app/src/main/java/com/jefmelo/mensageiro/activity/CadastrarTelefoneMainActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.TextUtils;
+
+import android.view.View;
+import android.widget.Toast;
+
+import com.jefmelo.mensageiro.databinding.ActivityCadastrarTelefoneMainBinding;
 import com.jefmelo.mensageiro.helper.Permissoes;
 
-import com.jefmelo.mensageiro.R;
+import com.jefmelo.mensageiro.util.MaskFormatUtil;
+
 
 public class CadastrarTelefoneMainActivity extends AppCompatActivity {
 
@@ -18,17 +27,61 @@ public class CadastrarTelefoneMainActivity extends AppCompatActivity {
             Manifest.permission.INTERNET
     };
 
+    private ActivityCadastrarTelefoneMainBinding telefoneBinding;
+
+    protected String telFormatado;
+
+    //Progressive Dialog
+    private ProgressDialog progressDialog;
+
+    ValidarCodigoMainActivity validarCodigoMainActivity = new ValidarCodigoMainActivity();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastrar_telefone_main);
+        telefoneBinding = ActivityCadastrarTelefoneMainBinding.inflate(getLayoutInflater());
+        setContentView(telefoneBinding.getRoot());
 
         Permissoes.validarPermissoes(1, this, permissoesNecessarias);
 
+        //Definição das Máscaras
+        telefoneBinding.editTexTelefone.addTextChangedListener(MaskFormatUtil.mask(telefoneBinding.editTexTelefone, MaskFormatUtil.FORMAT_FONE));
+        telefoneBinding.editTextCodPais.addTextChangedListener(MaskFormatUtil.mask(telefoneBinding.editTextCodPais, MaskFormatUtil.FORMAT_COD_PAIS));
+        telefoneBinding.editTextCodArea.addTextChangedListener(MaskFormatUtil.mask(telefoneBinding.editTextCodArea, MaskFormatUtil.FORMAT_COD_AREA));
+
+        //Tornar visível a activity
+        telefoneBinding.idActivityCadastrarTelefone.setVisibility(View.VISIBLE);
+
+        //Bloqueio de touch durante progresso
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Aguarde...");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+
+        telefoneBinding.btnCadastrar.setOnClickListener(v -> {
+            String nomeUsuario = telefoneBinding.editTextNome.getText().toString();
+            String telSemFormatar = telefoneBinding.editTextCodPais.getText().toString()
+                    + telefoneBinding.editTextCodArea.getText().toString()
+                    + telefoneBinding.editTexTelefone.getText().toString();
+
+            telFormatado = "+" + MaskFormatUtil.unmask(telSemFormatar);
+
+            progressDialog.setMessage("Verificando Número do Telefone...");
+            progressDialog.show();
+
+            if (TextUtils.isEmpty(telFormatado)) {
+                Toast.makeText(this, nomeUsuario + " Por Favor, Digite o Telefone...", Toast.LENGTH_LONG).show();
+            } else {
+                validarCodigoMainActivity.startPhoneNumberVerification(telFormatado);
+                Intent intent = new Intent(CadastrarTelefoneMainActivity.this, ValidarCodigoMainActivity.class);
+                intent.putExtra("numeroTelefone", telFormatado);
+                startActivity(intent);
+                //finish();
+            }
+        });
     }
 
-
-    //Requerimento de Permissões
+    //Inicio: Requerimento de Permissões
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -53,8 +106,13 @@ public class CadastrarTelefoneMainActivity extends AppCompatActivity {
 
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
-    }
+    }//Final: Requerimento de Permissões
+
 }
 
+//45min
 //https://www.youtube.com/watch?v=F_UemS493IM&t=645s
 //https://firebase.google.com/docs/auth/android/phone-auth?authuser=0
+
+//Versão 3h
+//https://youtu.be/F_UemS493IM?t=1548
